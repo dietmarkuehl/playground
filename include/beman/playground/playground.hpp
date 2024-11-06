3d2694e1c9832acf141591d8a0b5f7b0586c5cf3
Dietmar Kühl <dietmar.kuehl@me.com>
2024-11-06 00:16:29 +0000

    some basic content

---
+++ include/beman/playground/playground.hpp
@@ -0,0 +1,23 @@
+/*!
+ * \mainpage Playground to get Doxy up
+ *
+ * For now this project merely tries to get doxygen output on
+ * github pages
+ */
+
+/*!
+ * \namespace playground
+ * \brief The main playground namespace
+ * \headerfile beman/playground.hpp <beman/playground.hpp>
+ *
+ * Classes growing up play here.
+ */
+namespace playground
+{
+    /*!
+     * \brief The kid on the ground
+     */
+    class kid
+    {
+    };
+}
